c577e1594f56335ff74733d6dca127268b0f1c82
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-02-15 10:50:50 -0700

    Examples of Vue.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
+var app1 = new Vue({
+  el: '#app1',
+  data: {
+    message: "Hello from the View App.",
+    greeting: "My name is Carl."
+  }
+});
+
+// Equalvalent Jquery method
+// $('#showBtn').on('click', function () {
+  // $('myHead').toggle();
+// });
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    show: false
+  }
+});
+
+// v-for="t in todos" === todos.map(function (t) {})
+
 var ts = [
   { text: 'Learn JavaScript' },
   { text: 'Learn Vue' },
   { text: 'Build something awesome' }
 ];
+
+var app3 = new Vue({
+  el: '#app3',
+  data: {todos: ts}
+})
 
 var fishies = [
   { type: "halibut", ocean: "pacific" },
@@ -18,3 +45,33 @@ var fishies = [
   { type: "one fish", ocean: "arctic" },
 ];
 
+var pacificFish = [];
+for (var i=0; i<fishies.length; i++) {
+  if (fishies[i].ocean === 'pacific') {
+    pacificFish.push(fishies[i]);
+  }
+}
+
+var caribbeanFish = fishies.filter(function (item) {
+  return item.ocean === 'caribbean';
+});
+
+var app4 = new Vue({
+  el: '#app4',
+  data: {
+    fishData: [
+      {
+        title: 'All Fish',
+        data: fishies
+      },
+      {
+        title: "Pacific Fish",
+        data: pacificFish
+      },
+      {
+        title: "Caribbean Fish",
+        data: caribbeanFish
+      }
+    ]
+  }
+});
